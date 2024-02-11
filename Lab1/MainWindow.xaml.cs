01c25f9852f6a97ef9e6ee451631d0c0f6fa79b3
AsdZy <99485278+DmitryIldyukov@users.noreply.github.com>
2024-02-11 21:52:37 +0300

    Исправил буквы и добавил анимацию

--- Lab1/MainWindow.xaml.cs
+++ Lab1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,9 +10,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Lab1
 {
@@ -23,6 +26,38 @@ namespace Lab1
         public MainWindow()
         {
             InitializeComponent();
+
+            LetterJumpAnimation(FirstName, 0);
+            LetterJumpAnimation(Surname, 1);
+            LetterJumpAnimation(Patronymic, 2);
+        }
+
+        private void LetterJumpAnimation(Canvas letter, double delay)
+        {
+            foreach (UIElement el in letter.Children)
+            {
+                if (el is Rectangle || el is Ellipse)
+                {
+                    DoubleAnimation animation = new DoubleAnimation
+                    {
+                        From = Canvas.GetTop(el),
+                        To = Canvas.GetTop(el) + 50,
+                        Duration = TimeSpan.FromSeconds(0.5),
+                        AutoReverse = true,
+                        RepeatBehavior = RepeatBehavior.Forever,
+                        BeginTime = TimeSpan.FromSeconds(delay * 0.1),
+                    };
+
+                    Storyboard.SetTarget(animation, el);
+                    Storyboard.SetTargetProperty(animation, new PropertyPath(Canvas.TopProperty));
+
+                    Storyboard storyboard = new Storyboard();
+                    storyboard.Children.Add(animation);
+
+                    storyboard.Begin();
+                }
+            }
+            
         }
     }
 }
